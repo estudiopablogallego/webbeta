87a42df866e731fc62f0215b0c0fd1498f042c49
Toni Castillo <tonicastillo@gmail.com>
2021-01-18 10:54:31 +0100

    favicon fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,10 +66,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#999999`,
+        theme_color: `#66666`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/epg-favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
