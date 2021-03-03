cff1f34039247a5360cce3d4e363232e65237c77
Toni Castillo <tonicastillo@gmail.com>
2021-03-03 18:02:08 +0100

    fix y varios

--- src/context/cursorContext.js
+++ src/context/cursorContext.js
@@ -3,10 +3,10 @@ import React, { createContext } from "react"
 const defaultState = {
   cursor: "default", //left, right, pointer
   blanco: false,
-  posicion: {
-    x: -1000,
-    y: -1000,
-  },
+  // posicion: {
+  //   x: -1000,
+  //   y: -1000,
+  // },
   accionSinDefinir: () => {},
 }
 
@@ -29,9 +29,9 @@ class CursorProvider extends React.Component {
       this.setState({ cursor: cursorMode })
     }
   }
-  setPosicion = ({ x, y }) => {
-    this.setState({ posicion: { x, y } })
-  }
+  // setPosicion = ({ x, y }) => {
+  //   this.setState({ posicion: { x, y } })
+  // }
 
   componentDidMount() {
     // // Getting dark mode value from localStorage!
@@ -50,9 +50,9 @@ class CursorProvider extends React.Component {
         value={{
           cursor: this.state.cursor,
           blanco: this.state.blanco,
-          posicion: this.state.posicion,
+          // posicion: this.state.posicion,
           setBlanco: this.setBlanco,
-          setPosicion: this.setPosicion,
+          // setPosicion: this.setPosicion,
           setCursor: this.setCursor,
         }}
       >
