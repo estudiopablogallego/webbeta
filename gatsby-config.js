4aa2169450558175579f0550d30a48a2dd0dc8e5
Toni Castillo <tonicastillo@gmail.com>
2021-04-19 18:02:44 +0200

    no coookies no facebook

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,21 +41,21 @@ module.exports = {
         layout: require.resolve(`./src/components/layout.js`),
       },
     },
-    {
-      resolve: `gatsby-plugin-gdpr-cookies`,
-      options: {
-        googleAnalytics: {
-          trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-          // Setting this parameter is optional
-          anonymize: true,
-        },
-        facebookPixel: {
-          pixelId: "YOUR_FACEBOOK_PIXEL_ID",
-        },
-        // Defines the environments where the tracking should be available  - default is ["production"]
-        environments: ["production", "development"],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-gdpr-cookies`,
+    //   options: {
+    //     googleAnalytics: {
+    //       trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+    //       // Setting this parameter is optional
+    //       anonymize: true,
+    //     },
+    //     facebookPixel: {
+    //       pixelId: "YOUR_FACEBOOK_PIXEL_ID",
+    //     },
+    //     // Defines the environments where the tracking should be available  - default is ["production"]
+    //     environments: ["production", "development"],
+    //   },
+    // },
     // `gatsby-plugin-sitemap`,
     // `gatsby-plugin-robots-txt`,
     `gatsby-transformer-sharp`,
